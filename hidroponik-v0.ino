491cde91ba7a79242b4d90b034a5e5ace2bc8fb3
bekirr12 <bekirsamikaratas@gmail.com>
2026-07-04 23:13:09 +0300

    Add DHT22 temperature and humidity reading

--- hidroponik-v0.ino
+++ hidroponik-v0.ino
@@ -1,12 +1,19 @@
 #include <Arduino.h>
+#include <DHT.h>
 
 // pin definitions
 #define PUMP_RELAY_PIN 5
 #define LED_RELAY_PIN 18
+#define DHT_PIN 4
+
+// DHT type
+#define DHT_TYPE DHT22
 
 // Relay polarity: active-LOW module
 #define RELAY_ON  LOW
 #define RELAY_OFF HIGH
+
+DHT dht(DHT_PIN, DHT_TYPE);
 
 void setup() {
   Serial.begin(115200);
@@ -17,10 +24,22 @@ void setup() {
   digitalWrite(PUMP_RELAY_PIN, RELAY_OFF);
   digitalWrite(LED_RELAY_PIN, RELAY_OFF);
 
+  dht.begin();
+
   Serial.println("Setup complete, starting relays test");
 }
 
 void loop() {
+  float temperature = dht.readTemperature();
+  float humidity    = dht.readHumidity();
+
+  if (isnan(temperature) || isnan(humidity)) {
+    Serial.println("DHT read failed");
+  } else {
+    Serial.printf("Temperature: %.1f C, Humidity: %.1f %%\n", temperature, humidity);
+  }
+  delay(2000);
+
   digitalWrite(PUMP_RELAY_PIN, RELAY_ON);
   digitalWrite(LED_RELAY_PIN, RELAY_ON);
   Serial.println("Pump and Led ON");
