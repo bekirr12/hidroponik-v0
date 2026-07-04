5f2864133b31e498adbb8d70911c3b143c171781
bekirr12 <bekirsamikaratas@gmail.com>
2026-07-04 22:46:27 +0300

    Add pump relay pin control with active-LOW polarity

--- hidroponik-v0.ino
+++ hidroponik-v0.ino
@@ -1,10 +1,25 @@
+#include <Arduino.h>
+
+// pin definition
+#define PUMP_RELAY_PIN 5
+
+// Relay polarity: active-LOW module
+#define RELAY_ON  LOW
+#define RELAY_OFF HIGH
+
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
+  pinMode(PUMP_RELAY_PIN, OUTPUT);
+  digitalWrite(PUMP_RELAY_PIN, RELAY_OFF);
+  Serial.println("Setup complete, starting relay test");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.println("Hello");
-  delay(3000);
+  digitalWrite(PUMP_RELAY_PIN, RELAY_ON);
+  Serial.println("Pump ON");
+  delay(2000);
+
+  digitalWrite(PUMP_RELAY_PIN, RELAY_OFF);
+  Serial.println("Pump OFF");
+  delay(2000);
 }
