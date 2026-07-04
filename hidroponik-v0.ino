e81c63c74770e58b6ad2a59f729eb0dd29dcb5ed
bekirr12 <bekirsamikaratas@gmail.com>
2026-07-04 23:25:37 +0300

    Add HC-SR04 water level sensor reading

--- hidroponik-v0.ino
+++ hidroponik-v0.ino
@@ -5,6 +5,8 @@
 #define PUMP_RELAY_PIN 5
 #define LED_RELAY_PIN 18
 #define DHT_PIN 4
+#define TRIG_PIN 17
+#define ECHO_PIN 16
 
 // DHT type
 #define DHT_TYPE DHT22
@@ -13,16 +15,40 @@
 #define RELAY_ON  LOW
 #define RELAY_OFF HIGH
 
+// speed of sound in air, cm per microsecond
+#define SOUND_SPEED_CM_US 0.0343
+
 DHT dht(DHT_PIN, DHT_TYPE);
+
+float readWaterLevelCm() {
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+
+  long duration = pulseIn(ECHO_PIN, HIGH, 30000);
+
+  if (duration == 0) {
+    return -1;  // sentinel value: reading failed
+  }
+
+  return (duration * SOUND_SPEED_CM_US) / 2.0;
+}
 
 void setup() {
   Serial.begin(115200);
 
   pinMode(PUMP_RELAY_PIN, OUTPUT);
-  pinMode(LED_RELAY_PIN, OUTPUT);
-
   digitalWrite(PUMP_RELAY_PIN, RELAY_OFF);
+
+  pinMode(LED_RELAY_PIN, OUTPUT);
   digitalWrite(LED_RELAY_PIN, RELAY_OFF);
+
+  pinMode(TRIG_PIN, OUTPUT);
+  digitalWrite(TRIG_PIN, LOW);
+
+  pinMode(ECHO_PIN, INPUT);
 
   dht.begin();
 
@@ -37,6 +63,13 @@ void loop() {
     Serial.println("DHT read failed");
   } else {
     Serial.printf("Temperature: %.1f C, Humidity: %.1f %%\n", temperature, humidity);
+  }
+
+  float waterDistance = readWaterLevelCm();
+  if (waterDistance < 0) {
+    Serial.println("Water sensor read failed");
+  } else {
+    Serial.printf("Water distance: %.1f cm\n", waterDistance);
   }
   delay(2000);
 
